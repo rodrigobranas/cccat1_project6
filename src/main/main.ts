45ce13ccfddb0204fd4f71b850354ef2161f1c35
Rodrigo Branas <rodrigo.branas@gmail.com>
2021-06-29 15:50:13 -0300

    updated

--- src/main/main.ts
+++
@@ -1,5 +0,0 @@
-import RepositoryDatabaseFactory from "../adapter/factory/RepositoryDatabaseFactory";
-import HttpServer from "../infra/http/HttpServer";
-
-const repositoryFactory = new RepositoryDatabaseFactory();
-HttpServer.start(repositoryFactory);
